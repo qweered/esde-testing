7b4cae4ba986f74ff813166ea9c92760b6421ef5
qweered <grubian2@gmail.com>
2025-01-16 13:28:11 +0200

    Initial commit: Set up project structure with Node.js, Playwright, and Bun. Added .gitignore, package.json, TypeScript configuration, and example tests for a Todo application.

---
+++ flake.nix
@@ -0,0 +1,31 @@
+# run with
+# nix develop .
+{
+  description = "dev env";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { self, nixpkgs }: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs { inherit system; };
+  in {
+      devShells.${system}.default = pkgs.mkShell {
+      packages = with pkgs; [
+        nodejs
+        playwright-driver.browsers
+      ];
+
+      PLAYWRIGHT_NODEJS_PATH = "${pkgs.nodejs}/bin/node";
+      PLAYWRIGHT_BROWSERS_PATH=${pkgs.playwright-driver.browsers}
+      PLAYWRIGHT_SKIP_BROWSER_DOWNLOAD = 1;
+      #PLAYWRIGHT_SKIP_VALIDATE_HOST_REQUIREMENTS=true
+
+      shellHook = ''
+        echo "dev env started"
+        fish && echo "exit dev env" && exit
+      '';
+    };
+  };
+}
